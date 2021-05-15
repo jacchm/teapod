b2c50c1f9a76f54db3d43ad8e895ebd4ba8a1655
Jacek Chmiel <jacekchmiel03@gmail.com>
2021-05-15 17:34:39 +0200

    Initial commit - teapod loaded

---
+++ cmake-build-debug/CMakeFiles/reader_obj.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/reader_obj.dir/main.c.obj"
+  "libreader_obj.dll.a"
+  "reader_obj.exe"
+  "reader_obj.exe.manifest"
+  "reader_obj.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/reader_obj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
